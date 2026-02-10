024867212a324e99c1d98f9e61f5910b7409b2ae
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2026-02-10 14:13:15 +0500

    sign-in page html is added

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
 import Input from "../../components/Input";
+import { FaGoogle } from "react-icons/fa6";
 
 export default function SignIn() {
   return (
@@ -6,6 +9,15 @@ export default function SignIn() {
       <h4>Let’s Sign you in</h4>
       <p>Lorem ipsum dolor sit amet, consectetur</p>
       <Input label="Email Address" id="email-address" inputType="email" />
+      <Input label="Password" id="password" inputType="password" />
+      <p>Forgot Password</p>
+      <Button btnText="Sign In" />
+      <p>Don’t have an account? <Link to="/sign-up">Sign Up</Link></p>
+      <div>
+        <p>Or Sign In with</p>
+        <button>Google <FaGoogle /></button>
+      </div>
+      <p>By signing up you agree to our Terms and Conditions of Use</p>
     </div>
   )
 }
