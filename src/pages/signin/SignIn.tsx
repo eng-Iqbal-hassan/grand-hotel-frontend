7f75ec986d54989ac93a137a57f6c0b2016500a3
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2026-02-10 14:01:38 +0500

    authentication pages are added

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,7 +1,11 @@
+import Input from "../../components/Input";
+
 export default function SignIn() {
   return (
     <div>
-      This is signin screen
+      <h4>Let’s Sign you in</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur</p>
+      <Input label="Email Address" id="email-address" inputType="email" />
     </div>
   )
 }
